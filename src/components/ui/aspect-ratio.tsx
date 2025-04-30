cfe21b33d8c3e72134945fd32e656d75bc6e3af1
Jalil-sayarh <144999967+Jalil-sayarh@users.noreply.github.com>
2025-04-30 17:23:11 +0200

    11th test to fix layout

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -6,34 +6,31 @@ import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 const AspectRatio = React.forwardRef<
   React.ElementRef<typeof AspectRatioPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>
->(({ className, ...props }, ref) => {
-  return (
-    <div style={{ height: "100%" }}>
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          paddingBottom: `${100 / (props.ratio || 1)}%`,
-        }}
-        data-radix-aspect-ratio-wrapper=""
-      >
-        <AspectRatioPrimitive.Root
-          ref={ref}
-          {...props}
-          style={{
-            ...props.style,
-            position: "absolute",
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          }}
-        />
-      </div>
-    </div>
-  )
-})
+>(({ ...props }, ref) => (
+  <div
+    style={{
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      paddingBottom: `${100 / (props.ratio || 1)}%`,
+    }}
+    data-radix-aspect-ratio-wrapper=""
+  >
+    <div
+      ref={ref}
+      {...props}
+      style={{
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+      }}
+    />
+  </div>
+))
 AspectRatio.displayName = "AspectRatio"
 
 export { AspectRatio }
